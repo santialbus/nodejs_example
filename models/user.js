a303092c79d7cd144ed35f742b526d7ef0e3ea49
sbalbus <sbalbus@atsistemas.com>
2023-11-23 13:09:51 +0100

    nuevos cambios

--- models/user.js
+++ models/user.js
@@ -1,8 +1,12 @@
 const db = require('../config/config')
+const bcrypt = require('bcryptjs')
 
 const User = {};
 
-User.create = (user, result) => {
+User.create = async (user, result) => {
+
+    const hash = await bcrypt.hash(user.password, 10);
+
     const sql = `
         INSERT INTO  
             users(
@@ -27,7 +31,7 @@ User.create = (user, result) => {
             user.lastname,
             user.phone,
             user.image,
-            user.password,
+            hash,
             new Date(),
             new Date()
         ],
